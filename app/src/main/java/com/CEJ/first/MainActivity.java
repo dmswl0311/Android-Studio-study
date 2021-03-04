2728f69cd45214607daa4ab171e6a78ceb4126ec
dmswl0311 <dmswl_0311@naver.com>
2021-03-04 12:00:30 +0900

    Day2 - 버튼 클릭 이벤트, textview 변경 이벤트

--- app/src/main/java/com/CEJ/first/MainActivity.java
+++ app/src/main/java/com/CEJ/first/MainActivity.java
@@ -1,8 +1,11 @@
 package com.CEJ.first;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     //    멤버 변수 선언
     private final static String TAG="FIRST APP";
+    private LinearLayout button_eventLAY;
+    private TextView txt;
+
     //    멤버 메서드
     //    AppCompatActivity's method
     @Override
@@ -17,22 +23,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         
         // 사용자에게 보여지는 화면 설정
-        setContentView(R.layout.join);
+        setContentView(R.layout.button_event);
 
         // 디버깅
         Log.i(TAG,"onCreate()");
         Toast.makeText(MainActivity.this,"앱 실행",Toast.LENGTH_SHORT).show();
+
+//        joinLAY=findViewById(R.id.joinLAY);
+        button_eventLAY=findViewById(R.id.button_eventLAY);
+        txt=findViewById(R.id.textView6);
+
     }
 
     // onClick 속성 지원 Method
     // UI 요소 중 onClick 속성 설정한 경우 실행되는 메서드
 
     public void clickFunc(View v){
-        if (v.getId()==R.id.cancelBtn){
-            Toast.makeText(MainActivity.this,"취소",Toast.LENGTH_SHORT).show();
+        if (v.getId()==R.id.btn1){
+            Toast.makeText(MainActivity.this,"버튼1",Toast.LENGTH_SHORT).show();
+            button_eventLAY.setBackgroundColor(Color.BLUE);
+            txt.setText("BLUE");
         }
-        else if (v.getId()==R.id.okBtn){
-            Toast.makeText(MainActivity.this,"가입 완료",Toast.LENGTH_SHORT).show();
+        else if (v.getId()==R.id.btn2){
+            Toast.makeText(MainActivity.this,"버튼2",Toast.LENGTH_SHORT).show();
+            button_eventLAY.setBackgroundColor(Color.YELLOW);
+            txt.setText("YELLOW");
         }
     }
 }
